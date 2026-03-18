8e086f37c8378af6cf5b0fa8929694725a7f4fd4
FelixGames <rosangelapascoal12@gmail.com>
2026-03-18 15:37:55 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,9 +1,7 @@
-// Service Worker Minimalista para PWA (Ativação e Instalação)
 self.addEventListener('install', (e) => {
-  console.log('PWA Instalado!');
+    console.log("PWA Instalado!");
 });
 
 self.addEventListener('fetch', (e) => {
-  // Mantém o app funcionando online (não faz cache agressivo para evitar erros)
-  e.respondWith(fetch(e.request));
+    e.respondWith(fetch(e.request));
 });
