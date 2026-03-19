4c6f2533faefc1d87c38b4a8ac63a90cc6b479cb
FelixGames <rosangelapascoal12@gmail.com>
2026-03-19 19:58:42 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,15 +1,40 @@
-const CACHE_NAME = 'memevision-v4';
+const CACHE_NAME = 'memevision-pro-v1';
 const ASSETS = [
   './',
   './index.html',
-  'https://cdnjs.cloudflare.com/ajax/libs/html2canvas/1.4.1/html2canvas.min.js',
-  'https://fonts.googleapis.com/css2?family=Anton&display=swap'
+  './manifest.json',
+  './icon.png',
+  'https://unpkg.com/react@18/umd/react.production.min.js',
+  'https://unpkg.com/react-dom@18/umd/react-dom.production.min.js',
+  'https://unpkg.com/@babel/standalone/babel.min.js',
+  'https://cdn.tailwindcss.com',
+  'https://cdnjs.cloudflare.com/ajax/libs/html2canvas/1.4.1/html2canvas.min.js'
 ];
 
+// Instalação: Força o novo cache
 self.addEventListener('install', (e) => {
-  e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
+  self.skipWaiting();
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+  );
 });
 
-self.addEventListener('fetch', (e) => {
-  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
+// Ativação: Limpa caches antigos (Mata a v4)
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
+      );
+    })
+  );
 });
+
+// Estratégia: Network First (Tenta internet primeiro, se falhar usa cache)
+// Isso evita que o site fique travado em versões antigas
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    fetch(e.request).catch(() => caches.match(e.request))
+  );
+});
+
